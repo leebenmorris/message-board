23a38ab3df754dce45fc7ddddbf33642ce6d1ecd
leebenmorris <leebenmorris@gmail.com>
2023-01-08 09:16:30 +0000

    add get/all message endpoint

--- src/app.js
+++ src/app.js
@@ -10,4 +10,9 @@ app.post('/message/new', data.save, (req, res) => {
 	res.status(201).json({ ok: true })
 })
 
+app.get('/message/all', data.getAll, (req, res) => {
+	const { messages } = res.locals
+	res.status(200).json(messages)
+})
+
 export default app
