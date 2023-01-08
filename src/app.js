c596dc45299be0962ce8b66cd6d565fad02f827c
leebenmorris <leebenmorris@gmail.com>
2023-01-08 08:44:23 +0000

    fix app to pass save test

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from 'express'
 
+import data from './data.js'
+
 const app = express()
 
-app.post('/message/new', (req, res) => {
+app.use(express.json())
+
+app.post('/message/new', data.save, (req, res) => {
 	res.status(201).json({ ok: true })
 })
 
